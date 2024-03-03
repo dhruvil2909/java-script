c4c1a63b419fe2085c5828a08f2d08ba6ff0f008
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-03 17:09:49 +0000

    variables

---
+++ dhruvil/variable.js
@@ -0,0 +1,12 @@
+const accountid=7234
+let accountEmail="dhruvil@google.com"
+var accountPassword="2909"
+accountcity="veraval"
+
+
+accountEmail="dhruv@google.com"
+accountPassword="29"
+accountcity="rajkot"
+
+//console.log(accountid);
+console.table([accountid,accountEmail,accountPassword,accountcity]);
