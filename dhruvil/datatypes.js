4f292d0e7989534353be282fbc799630e6ff4fd0
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-03 17:10:08 +0000

    datatypes

---
+++ dhruvil/datatypes.js
@@ -0,0 +1,12 @@
+//number => 2 to power 52
+//bigint
+//string => ""
+//boolen => true/false
+//null => standalone value
+//undefind =>
+//symbol => unique
+
+//object
+
+console.log(typeof "dhruvil");
+console.log(typeof 13);
