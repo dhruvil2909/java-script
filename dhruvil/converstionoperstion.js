cf571760be9bae632adb0a4bc8c1b17e54b4ec08
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-04 18:51:47 +0000

    converstion

---
+++ dhruvil/converstionoperstion.js
@@ -0,0 +1,25 @@
+let score="dhruvil"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+
+//33 =>33
+//"33abc =>nan(not and number"
+//true=>1 ;false=>0
+
+let isLoggedIn=0
+
+let booleanIsloggedIn= Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// 1=>true; 0=>false
+//""=> false "dhruvil"=>true
+
+let someNumber=2909
+let stringNumber= String(someNumber)
+console.log(stringNumber);
