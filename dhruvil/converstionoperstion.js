fb103d0bd78c76e24628f27ba0b2a00442952c74
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-06 18:14:57 +0000

    converstionopertion

--- dhruvil/converstionoperstion.js
+++ dhruvil/converstionoperstion.js
@@ -1,11 +1,11 @@
 let score="dhruvil"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueinnumber=Number(score)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
 
 
 //33 =>33
@@ -15,11 +15,39 @@ console.log(valueinnumber);
 let isLoggedIn=0
 
 let booleanIsloggedIn= Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 
 // 1=>true; 0=>false
 //""=> false "dhruvil"=>true
 
 let someNumber=2909
 let stringNumber= String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+
+//----------------operation-----------------
+//console.log(1+2);
+//console.log("1"+2);
+//console.log(1+"2");
+
+//console.log((12+2)*2+3);
+
+let str1="dhruvil"
+let str2=" prajapati"
+let str3=str1 + str2
+
+//console.log(str3);
+
+let gamescore=20
+gamescore++;
+
+//console.log(gamescore)
+
+let gamescore2=20
+++gamescore2;
+//console.log(gamescore2)
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+console.log(2**3);
