f30da630a2259f13d1af7dc23a27973915dccd55
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-08 20:17:42 +0000

    comparison

--- dhruvil/comparison.js
+++ dhruvil/comparison.js
@@ -3,3 +3,16 @@
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===//
+console.log("2" === 2);
+console.log(2 === 2);
+// console.log("2" === "3");
