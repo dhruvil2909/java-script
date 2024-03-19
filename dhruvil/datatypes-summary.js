db8f4bfc946f45f0bc72e261d768e6434b1b05c4
dhruvil2909 <161968423+dhruvil2909@users.noreply.github.com>
2024-03-19 18:11:57 +0000

    datatypes-summary

--- dhruvil/datatypes-summary.js
+++ dhruvil/datatypes-summary.js
@@ -6,7 +6,7 @@
 // const scoreValue = 100.1
 
 // const isLoggedIn = true
-// const outsideTemp = null
+const outsideTemp = null
 
 // let useremail;
 
@@ -15,11 +15,22 @@ const anotherId = Symbol('123')
 
 //console.log(id === anotherId);
 
-
+// const bigNumber = 3456543576654356754n
 
 // non primitive( reference)
 // array, function, objects
 
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof outsideTemp);
 
 
 
